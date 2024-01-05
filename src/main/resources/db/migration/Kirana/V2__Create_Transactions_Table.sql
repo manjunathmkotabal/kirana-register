2aeb2937ed0f8d578a9db1f682f08cb70516c699
manjunathmkotabal <manjunathmkotabal99@gmail.com>
2024-01-05 19:30:40 +0530

    Adding env variables in compose

--- src/main/resources/db/migration/Kirana/V2__Create_Transactions_Table.sql
+++ src/main/resources/db/migration/Kirana/V2__Create_Transactions_Table.sql
@@ -1,6 +1,6 @@
 -- V1__Create_Transactions_Table.sql
 
-CREATE TABLE transactions (
+CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY,
     amountINR DECIMAL(10, 2),
     amountUSD DECIMAL(10, 2),
